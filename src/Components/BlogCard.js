d51a8885f7e3648a1f125890633663f2c29c1717
“BiankaFranke” <bianka_franke@hotmail.de>
2022-09-17 12:00:14 +0200

    alt-tag added

--- src/Components/BlogCard.js
+++ src/Components/BlogCard.js
@@ -6,15 +6,12 @@ import Btn from "./Btn";
 // Import CSS
 import "./BlogCard.css";
 
-// Import Data
-import blogData from "./BlogData";
-
 const BlogCard = (props) => {
     return ( 
         <div className="BlogCard">
             <div className="grid-item">
                 <div className="blogImg">
-                    <img src={props.img}></img>
+                    <img src={props.img} alt="Blog Img"></img>
                 </div>
                 <div className="blogDescription">
                     <h2 className="title">{props.title}</h2>
